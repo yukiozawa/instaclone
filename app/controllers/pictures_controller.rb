4eb4714b868dbf3ea0d68cd9b4f788e1ccd5933c
yuki <yukimomo001@gmail.com>
2018-01-27 16:34:03 +0900

    pictureの編集、削除機能の実装

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,13 +1,51 @@
 class PicturesController < ApplicationController
   def index
-    
+    @pictures = Picture.order(created_at: :desc).page(params[:page]).per(20)
   end
 
   def new
-    
+    @picture = Picture.new
   end
 
   def create
-    
+    # binding.pry
+    @picture = Picture.new(picture_params)
+  
+    if @picture.save
+      redirect_to root_path, notice: '投稿完了しました。'
+    else
+      redirect_to new_picture_path, alert: '投稿に失敗しました。'
+    end
+  end
+
+  def show
+    @picture = Picture.find(params[:id])
+  end
+
+  def edit
+    @picture = Picture.find(params[:id])
+  end
+
+  def update
+    # binding.pry
+    @picture = Picture.find(params[:id])
+    if @picture.update(picture_params)
+        redirect_to picture_path(@picture.id), notice: "ブログを編集しました！"
+    else
+        render 'edit'
+    end
+  end
+
+  def destroy
+    @picture = Picture.find(params[:id])
+    @picture.destroy
+
+    redirect_to root_path, notice:"Pictureを削除しました！"
+  end
+
+
+  private
+  def picture_params
+    params.require(:picture).permit(:title, :content, :image).merge(user_id: current_user.id)
   end
 end
