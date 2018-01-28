0652f5a3d7991b858f2e3c23c2a0c58a9d79605d
yuki <yukimomo001@gmail.com>
2018-01-28 15:37:09 +0900

    sendgrid_productionに追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  config.action_mailer.default_url_options = { host: 'http://fierce-mesa-27849.herokuapp.com/' }
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings =
+  {
+   user_name: ENV['SENDGRID_USERNAME'],
+   password: ENV['SENDGRID_PASSWORD'],
+   domain: "heroku.com",
+   address: "smtp.sendgrid.net",
+   port: 587,
+   authentication: :plain,
+   enable_starttls_auto: true
+}
 end
